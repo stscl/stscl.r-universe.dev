d4c404a857f18c7ec56f2ab8822ba32c79db8aeb
SpatLyu <lyu.geosocial@gmail.com>
2024-08-31 11:16:19 +0800

    add sdsfun package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,8 +1,9 @@
 setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
-  package = c("geocn"),
-  url = c("https://github.com/stscl/geocn"),
-  subdir = rep(NA,1)
+  package = c("geocn","sdsfun"),
+  url = c("https://github.com/stscl/geocn",
+          "https://github.com/stscl/sdsfun"),
+  subdir = rep(NA,2)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
