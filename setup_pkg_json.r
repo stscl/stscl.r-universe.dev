cf365a235520ace464924833510eebd429d8edeb
SpatLyu <lyu.geosocial@gmail.com>
2024-10-16 15:37:48 +0800

    add gdverse

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,9 +1,10 @@
 setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
-  package = c("geocn","sdsfun"),
+  package = c("geocn","sdsfun","gdverse"),
   url = c("https://github.com/stscl/geocn",
-          "https://github.com/stscl/sdsfun"),
-  subdir = rep(NA,2)
+          "https://github.com/stscl/sdsfun",
+          "https://github.com/stscl/gdverse"),
+  subdir = rep(NA,3)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
