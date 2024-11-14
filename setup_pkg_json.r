c8c5dae3284f467319dc0139d0236729b13243bb
SpatLyu <lyu.geosocial@gmail.com>
2024-11-14 08:37:00 +0800

    try to complie sesp for binary in r-universe

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,10 +1,11 @@
 setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
-  package = c("geocn","sdsfun","gdverse"),
+  package = c("geocn","sdsfun","gdverse","sesp"),
   url = c("https://github.com/stscl/geocn",
           "https://github.com/stscl/sdsfun",
-          "https://github.com/stscl/gdverse"),
-  subdir = rep(NA,3)
+          "https://github.com/stscl/gdverse",
+          "https://github.com/stscl/sesp"),
+  subdir = rep(NA,4)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
