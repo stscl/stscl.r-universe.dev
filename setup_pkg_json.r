325b4e0202fa3190b8b493fd81b27ccdbf72a432
SpatLyu <lyu.geosocial@gmail.com>
2024-11-15 23:30:04 +0800

    add cisp pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,11 +1,12 @@
 setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
-  package = c("geocn","sdsfun","gdverse","sesp"),
+  package = c("geocn","sdsfun","gdverse","sesp","cisp"),
   url = c("https://github.com/stscl/geocn",
           "https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
-          "https://github.com/stscl/sesp"),
-  subdir = rep(NA,4)
+          "https://github.com/stscl/sesp",
+          "https://github.com/stscl/cisp"),
+  subdir = rep(NA,5)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
