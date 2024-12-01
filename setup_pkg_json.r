69f512ce9253c7f6c453a55aef2f39a19110a717
SpatLyu <lyu.geosocial@gmail.com>
2024-12-01 15:37:35 +0800

    add `sshicm` pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,12 +1,14 @@
 setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
-  package = c("geocn","sdsfun","gdverse","sesp","cisp"),
+  package = c("geocn","sdsfun","gdverse",
+              "sesp","cisp","sshicm"),
   url = c("https://github.com/stscl/geocn",
           "https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
           "https://github.com/stscl/sesp",
-          "https://github.com/stscl/cisp"),
-  subdir = rep(NA,5)
+          "https://github.com/stscl/cisp",
+          "https://github.com/stscl/sshicm"),
+  subdir = rep(NA,6)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
