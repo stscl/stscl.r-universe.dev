e8596e228f2d5b50e8108c6e43df74a38b5fadb3
SpatLyu <lyu.geosocial@gmail.com>
2024-12-09 18:53:40 +0800

    add `spEDM` pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -2,13 +2,15 @@ setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
   package = c("geocn","sdsfun","gdverse",
-              "sesp","cisp","sshicm"),
+              "sesp","cisp","sshicm",
+              "spEDM"),
   url = c("https://github.com/stscl/geocn",
           "https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
           "https://github.com/stscl/sesp",
           "https://github.com/stscl/cisp",
-          "https://github.com/stscl/sshicm"),
-  subdir = rep(NA,6)
+          "https://github.com/stscl/sshicm",
+          "https://github.com/stscl/spEDM"),
+  subdir = rep(NA,7)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
