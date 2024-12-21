552772c68f07b17596ef64c2231c462872024ab8
SpatLyu <lyu.geosocial@gmail.com>
2024-12-21 22:09:51 +0800

    add `itmsa` pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -2,15 +2,16 @@ setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
   package = c("geocn","sdsfun","gdverse",
-              "sesp","cisp","sshicm",
-              "spEDM"),
+              "sesp","cisp","spEDM","itmsa",
+              "sshicm"),
   url = c("https://github.com/stscl/geocn",
           "https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
           "https://github.com/stscl/sesp",
           "https://github.com/stscl/cisp",
-          "https://github.com/stscl/sshicm",
-          "https://github.com/stscl/spEDM"),
-  subdir = rep(NA,7)
+          "https://github.com/stscl/spEDM",
+          "https://github.com/stscl/itmsa",
+          "https://github.com/stscl/sshicm"),
+  subdir = rep(NA,8)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
