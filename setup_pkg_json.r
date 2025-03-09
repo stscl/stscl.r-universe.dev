96cac0ec065c0955d516182a0482a0ab73ecfa36
SpatLyu <lyu.geosocial@gmail.com>
2025-03-09 13:22:33 +0800

    remove sesp pkg

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,17 +1,17 @@
 setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
 
 df = data.frame(
-  package = c("geocn","sdsfun","gdverse",
-              "sesp","cisp","spEDM","itmsa",
-              "sshicm"),
-  url = c("https://github.com/stscl/geocn",
-          "https://github.com/stscl/sdsfun",
+  package = c("sdsfun","gdverse","spEDM","itmsa",
+              "sshicm","geocn", "cisp"#, "sesp"
+              ),
+  url = c("https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
-          "https://github.com/stscl/sesp",
-          "https://github.com/stscl/cisp",
           "https://github.com/stscl/spEDM",
           "https://github.com/stscl/itmsa",
-          "https://github.com/stscl/sshicm"),
-  subdir = rep(NA,8)
+          "https://github.com/stscl/sshicm",
+          "https://github.com/stscl/geocn",
+          "https://github.com/stscl/cisp"#,"https://github.com/stscl/sesp",
+          ),
+  subdir = rep(NA,7)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
