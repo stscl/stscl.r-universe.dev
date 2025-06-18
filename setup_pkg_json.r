ec98a391ee619f8490758103e1ab18c56e6fb52d
SpatLyu <lyu.geosocial@gmail.com>
2025-06-18 19:15:55 +0800

    include `tEDM` package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,18 +1,19 @@
-setwd('c:/Users/dell/Downloads/pkgdev/stscl_r-uni/')
+setwd('d:/download/pkgdev/stscl.r-universe.dev/')
 
 df = data.frame(
-  package = c("sdsfun","gdverse","spEDM","itmsa",
+  package = c("sdsfun","gdverse","spEDM","tEDM","itmsa",
               "sshicm","geocn", "cisp", "sesp"
               ),
   url = c("https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
           "https://github.com/stscl/spEDM",
+          "https://github.com/stscl/tEDM",
           "https://github.com/stscl/itmsa",
           "https://github.com/stscl/sshicm",
           "https://github.com/stscl/geocn",
           "https://github.com/stscl/cisp",
           "https://github.com/stscl/sesp"
           ),
-  subdir = rep(NA,8)
+  subdir = rep(NA,9)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
