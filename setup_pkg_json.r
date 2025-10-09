46e58286de7ca03d50a5c6053bc55ed1ca3279a1
SpatLyu <lyu.geosocial@gmail.com>
2025-10-09 21:56:04 +0800

    include `infocausality` package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,19 +1,20 @@
-setwd('d:/download/pkgdev/stscl.r-universe.dev/')
+setwd('./pkgdev/stscl.r-universe.dev/')
 
 df = data.frame(
-  package = c("sdsfun","gdverse","spEDM","tEDM","itmsa",
-              "sshicm","geocn", "cisp", "sesp"
+  package = c("sdsfun","gdverse","spEDM","tEDM","infocausality",
+              "itmsa", "sshicm", "geocn", "cisp", "sesp"
               ),
   url = c("https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
           "https://github.com/stscl/spEDM",
           "https://github.com/stscl/tEDM",
+          "https://github.com/stscl/infocausality",
           "https://github.com/stscl/itmsa",
           "https://github.com/stscl/sshicm",
           "https://github.com/stscl/geocn",
           "https://github.com/stscl/cisp",
           "https://github.com/stscl/sesp"
           ),
-  subdir = rep(NA,9)
+  subdir = rep(NA,10)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
