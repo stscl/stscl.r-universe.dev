4df6a84233f79df33be7e6823ee53fb5948b2209
SpatLyu <lyu.geosocial@gmail.com>
2026-04-02 00:00:01 +0800

    include infoxtr package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -1,13 +1,15 @@
 setwd('./pkgdev/stscl.r-universe.dev/')
 
 df = data.frame(
-  package = c("sdsfun","gdverse","spEDM","tEDM","infocausality",
-              "itmsa", "sshicm", "geocn", "cisp", "sesp"
+  package = c("sdsfun", "gdverse", "spEDM", "tEDM", "infoxtr",
+              "infocausality", "itmsa", "sshicm", "geocn", 
+              "cisp", "sesp"
               ),
   url = c("https://github.com/stscl/sdsfun",
           "https://github.com/stscl/gdverse",
           "https://github.com/stscl/spEDM",
           "https://github.com/stscl/tEDM",
+          "https://github.com/stscl/infoxtr",
           "https://github.com/stscl/infocausality",
           "https://github.com/stscl/itmsa",
           "https://github.com/stscl/sshicm",
@@ -15,6 +17,6 @@ df = data.frame(
           "https://github.com/stscl/cisp",
           "https://github.com/stscl/sesp"
           ),
-  subdir = rep(NA,10)
+  subdir = rep(NA,11)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
