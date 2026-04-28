9c20d148e2e21b7ccffd7d6ceddacb151140dbcd
SpatLyu <lyu.geosocial@gmail.com>
2026-04-29 00:00:01 +0800

    include `pc` package

--- setup_pkg_json.r
+++ setup_pkg_json.r
@@ -2,7 +2,7 @@
 
 df = data.frame(
   package = c("sdsfun", "gdverse", "spEDM", "tEDM", "infoxtr",
-              "infocausality", "itmsa", "sshicm", "geocn", 
+              "pc", "infocausality", "itmsa", "sshicm", "geocn", 
               "cisp", "sesp"
               ),
   url = c("https://github.com/stscl/sdsfun",
@@ -10,6 +10,7 @@ df = data.frame(
           "https://github.com/stscl/spEDM",
           "https://github.com/stscl/tEDM",
           "https://github.com/stscl/infoxtr",
+          "https://github.com/stscl/pc",
           "https://github.com/stscl/infocausality",
           "https://github.com/stscl/itmsa",
           "https://github.com/stscl/sshicm",
@@ -17,6 +18,6 @@ df = data.frame(
           "https://github.com/stscl/cisp",
           "https://github.com/stscl/sesp"
           ),
-  subdir = rep(NA,11)
+  subdir = rep(NA,12)
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
